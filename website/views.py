267cfda2e8e82458db29c371637b34beb96cfdc7
ZachWalden <zachwalden2@outlook.com>
2023-05-13 18:47:53 -0500

    table view added, will later fill with info from db

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route("/")   #@name of blueprint
 def home():
     return render_template("home.html")
+
+@views.route('/table')
+def table():
+    return render_template("table.html")
